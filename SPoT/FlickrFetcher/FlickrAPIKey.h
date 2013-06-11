20f585a97b33001ccf608ea6bc538d248e512c39
Chris Chapman <chrischapman900@gmail.com>
2013-06-11 03:21:57 -0400

    Initial Commit

---
+++ SPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,12 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"869f8f88c41ab9e97499f5608d319957"
